e5a8243866def06674d47930ea083450aeb0df2b
OlehHorchyn <ohorchyn@gmail.com>
2019-04-11 17:09:55 +0300

    Add project files.

---
+++ WebApplication1_CVK/Model/ResToChild.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary1_CVK.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1_CVK.Model
+{
+    public class ResToChild
+    {
+        public string NumOkrug { set; get; }
+        public List<Department> departments { set; get; }
+
+        public ResToChild()
+        {
+        }
+    }
+}
